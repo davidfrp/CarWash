8a132e2f0df2ad8322c6e7d06f0c73ff296df8a3
Jonas Gehrke <jonasegehrke@gmail.com>
2020-10-13 16:08:17 +0200

    removed code to start from scratch

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,12 +1,3 @@
 public class Statistics {
-    private int economy;
-    private int standard;
-    private int deLuxe;
 
-
-    public String getStats() {
-        return "Economy: " + economy + "\n" +
-                "Standard: " + standard + "\n" +
-                "De Luxe: "+ deLuxe;
-    }
 }
--- src/WashCard.java
+++ src/WashCard.java
@@ -1,21 +1,3 @@
 public class WashCard {
-    private double balance;
-    private String name;
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public WashCard(double balance, String name) {
-        this.balance = balance;
-        this.name = name;
-    }
 }
